b455872e10d8856be14fcae1a69eb04533e8d04f
AdamDono <111118088+AdamDono@users.noreply.github.com>
2023-07-24 12:39:34 +0200

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { LandingComponent } from './landing/landing.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'landing', component: LandingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
